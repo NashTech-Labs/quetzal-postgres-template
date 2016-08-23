104471419ee5f006117ed9e026afd07687a7f387
sandy <phalodi@gmail.com>
2016-08-23 18:42:21 +0530

    test parser method toSQLQuery to check its convert into sql or not

--- quetzal-postgres/src/main/scala/com/knoldus/quetzal/Quetzal.scala
+++ quetzal-postgres/src/main/scala/com/knoldus/quetzal/Quetzal.scala
@@ -1,5 +1,12 @@
 package com.knoldus.quetzal
 
-object Quetzal extends App{
-  println("Quetzal Postgres")
+import com.ibm.research.rdf.store.query
+import com.ibm.research.rdf.store.sparql.Parser
+
+class Quetzal {
+  def main(args: Array[String]) {
+    println("Quetzal Postgres")
+    val sql = Parser.toSQLQuery("select count(*) {  ?s ?p ?o }")
+    println("sql query is " + sql)
+  }
 }
